bbf834da7755a149d1c04902725faaae586fb6f2
Connor Weinke <weinke21@live.com>
2024-10-26 23:37:25 -0500

    Completed Exam 2

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
-Access-Control-Allow-Origin: *
+
 // import the utility functions "decodeHtml" and "shuffle"
-import { resolve } from 'path';
+//import { resolve } from 'path';
 import { decodeHtml, shuffle } from './utils.js' 
 
 // get the elements from the DOM
@@ -19,13 +19,44 @@ const nextQuestionElement = document.querySelector('#nextQuestion')
 		const { question, correct_answer: correct, incorrect_answers: incorrect } = json.results[0]
 		const answers = shuffle([ ...incorrect, correct ])
 		return { question, answers, correct }
-	}
 		
-	console.log(await getNextQuestion())
+	}
 
 	// todo: create your "renderQuestion" function
+	const renderQuestion = ({ question, answers, correct }) => {
+		questionElement.innerHTML = ''
+		answersElement.innerHTML = ''
+		questionElement.textContent = decodeHtml(question)
+		answers.forEach((answers) => {
+	
+			const answerButton = document.createElement("button")
+			answerButton.innerHTML = decodeHtml(answers)
+			answersElement.appendChild(answerButton)
+			
+			answerButton.addEventListener("click", () => {
+				if (answers === correct) {
+				button.classList.add('correct')
+				answersElement.querySelectorAll('button').forEach(b => b.disabled = true)
+				alert('Correct!')
+				return
+			}
 
+				button.disabled = true
+				alert('Incorrect!')});
+		})
+		
+	 }
+	
+		
 	// todo: add the event listener to the "nextQuestion" button
+	const next = async () => {
+		
+		renderQuestion(await getNextQuestion())
+		nextQuestionElement.disabled = true
+		setTimeout(() => nextQuestionElement.disabled = false, 10000)
+	}
+	 const button = document.querySelector('#nextQuestion')
+	button.addEventListener("click", (next));
 
 })()
 
